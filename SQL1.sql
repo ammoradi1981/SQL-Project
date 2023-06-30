f39ad4256e72bec83721dbbfa18c3c300a8df778
ammoradi1981 <137802656+ammoradi1981@users.noreply.github.com>
2023-06-30 10:47:43 -0400

    SQL2
    SQL2

--- SQL1.sql
+++ SQL1.sql
@@ -1,3 +1,12 @@
 
-SELECT * FROM actor;
+SELECT * FROM  staff;
+select*from category;
+select*from city;
+select*from country;
+
+SELECT Staff.staff_id, Category.name
+FROM Staff
+INNER JOIN Category
+ON Staff.staff_id = Category.category_id;
+
 
