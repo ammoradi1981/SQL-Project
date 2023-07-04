40384514bf93bb827bd59b72e74aeecb2a03f546
ammoradi1981 <137802656+ammoradi1981@users.noreply.github.com>
2023-07-04 12:41:34 -0400

    Create SQL3.sql
    sql 3

---
+++ SQL3.sql
@@ -0,0 +1,67 @@
+select*from payment
+
+select min (amount) from payment;
+select max (amount) from payment;
+select STDDEV (amount) from payment;
+select SUM (amount) from payment;
+select AVG (amount) from payment;
+select count (amount) from payment;
+select Variance (amount) from payment;
+
+
+select*from payment 
+order by customer_id
+
+--to calculate the sum of each customer_id
+select customer_id, sum(amount) as Sum_amount
+from payment
+group by customer_id
+order by customer_id
+
+--To calculate the sum of a range of values from "a" to "b" 
+select sum(amount) as total_sum
+from payment 
+where customer_id>=1 and customer_id<2; 
+
+--The window function, to performs calculations across a set of rows
+select customer_id,amount, sum (amount) over(order by customer_id) as Total_amount
+from payment
+
+select customer_id,amount, sum (amount) over my_window as cumulative_amount
+from payment p
+WINDOW
+  my_window AS (PARTITION BY p.customer_id order by p.customr_id)
+  
+
+
+
+SELECT
+  p.customer_id,
+  r.rental_id,
+  r.rental_date, p.amount,
+  SUM(p.amount) OVER my_window AS cumulative_revenue,
+  AVG(p.amount) OVER my_window AS average_revenue,
+  MAX(p.amount) OVER my_window AS max_payment,
+  sum(p.amount) over(order by p.customer_id) as Total_amount
+FROM
+  payment p
+JOIN rental r ON p.rental_id = r.rental_id
+WINDOW
+  my_window AS (PARTITION BY p.customer_id ORDER BY r.rental_date)
+ORDER BY
+  customer_id, rental_date;
+  
+
+
+  
+  
+ 
+
+
+
+
+
+
+
+
+
